ebcd06a64b8a8989f263f50d3f8cda1f3c60303b
Eli Hayon <dyslexicanaboko@gmail.com>
2020-02-03 22:35:08 -0500

    This is a file that should have been checked in but somehow got left out. Something weird is happening with this project's ignore file because this keeps happening.

---
+++ SimpleSmtpInterceptor.Lib/Parsers/HeaderOnlyParser.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace SimpleSmtpInterceptor.Lib.Parsers
+{
+    public class HeaderOnlyParser
+        : EmailParser
+    {
+        public HeaderOnlyParser(TextReader reader, bool verboseOutput) 
+            : base(reader, verboseOutput)
+        {
+        }
+
+        public override void ParseBody()
+        {
+            
+        }
+    }
+}
