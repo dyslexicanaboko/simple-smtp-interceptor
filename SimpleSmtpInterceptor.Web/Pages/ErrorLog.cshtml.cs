7fd6c69065caa522b78d61340b622ea64b29bbfb
Eli Hayon <dyslexicanaboko@gmail.com>
2020-02-16 14:05:14 -0500

    Saving razor page before continuing.

---
+++ SimpleSmtpInterceptor.Web/Pages/ErrorLog.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SimpleSmtpInterceptor.Data;
+using SimpleSmtpInterceptor.Data.Entities;
+
+namespace SimpleSmtpInterceptor.Web.Pages
+{
+    public class ErrorLogModel : PageModel
+    {
+        private readonly SimpleSmtpInterceptor.Data.InterceptorModel _context;
+
+        public ErrorLogModel(SimpleSmtpInterceptor.Data.InterceptorModel context)
+        {
+            _context = context;
+        }
+
+        public IList<Log> Log { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Log = await _context
+                .Logs
+                .OrderByDescending(x => x.CreatedOnUtc)
+                .Take(20)
+                .ToListAsync();
+        }
+    }
+}
